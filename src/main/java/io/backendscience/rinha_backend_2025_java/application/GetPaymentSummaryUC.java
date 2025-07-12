d8e80f2da83c9e7b97f8fa6413afb110c0d9548e
lecosas <lecosas@gmail.com>
2025-07-12 14:42:12 -0300

    feat(h2): Remove H2

--- src/main/java/io/backendscience/rinha_backend_2025_java/application/GetPaymentSummaryUC.java
+++ src/main/java/io/backendscience/rinha_backend_2025_java/application/GetPaymentSummaryUC.java
@@ -1,7 +1,5 @@
 package io.backendscience.rinha_backend_2025_java.application;
 
-import io.backendscience.rinha_backend_2025_java.adapter.inbound.PaymentsController;
-import io.backendscience.rinha_backend_2025_java.adapter.outbound.PaymentRepository;
 import io.backendscience.rinha_backend_2025_java.adapter.outbound.resources.SummaryPaymentResponse;
 import io.backendscience.rinha_backend_2025_java.domain.PaymentSummaryTotal;
 import lombok.RequiredArgsConstructor;
@@ -16,56 +14,16 @@ import java.time.OffsetDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.logging.Logger;
 
 @Component
 @RequiredArgsConstructor
 public class GetPaymentSummaryUC {
 
-    private final PaymentRepository paymentRepository;
     private final Logger logger = Logger.getLogger(GetPaymentSummaryUC.class.getName());
     private final JedisPooled jedisPooled;
 
-//    public Map<Integer, PaymentSummaryTotal> execute(String from, String to) {
-//        Map<Integer, PaymentSummaryTotal> map = new HashMap<>();
-//        List<SummaryPaymentResponse> res;
-//
-//        OffsetDateTime fromOffset;
-//        OffsetDateTime toOffset;
-//
-//        if (from != null && !from.isBlank()) {
-//            from = from.endsWith("Z") ? from : from + "Z";
-//            fromOffset = OffsetDateTime.parse(from);
-//        } else {
-//            fromOffset = null;
-//        }
-//
-//        if (to != null && !to.isBlank()) {
-//            to = to.endsWith("Z") ? to : to + "Z";
-//            toOffset = OffsetDateTime.parse(to);
-//        } else {
-//            toOffset = null;
-//        }
-//
-//        res =  paymentRepository.sumAmountBetween(fromOffset, toOffset);
-//
-//        for (SummaryPaymentResponse info : res) {
-//            Integer type = info.type();
-//            BigDecimal sum = info.sum();
-//            Long count = info.count();
-//            map.put(type, new PaymentSummaryTotal(count, sum));
-//        }
-//
-//        return map;
-//    }
-
     public Map<Integer, PaymentSummaryTotal> execute(String from, String to) {
-
-
-//        TSRangeParams rangeParams = TSRangeParams
-//                .rangeParams();
-
         Map<Integer, PaymentSummaryTotal> map = new HashMap<>();
         List<SummaryPaymentResponse> res;
 
@@ -105,13 +63,6 @@ public class GetPaymentSummaryUC {
 
         map.put(0, new PaymentSummaryTotal(countDefault, BigDecimal.valueOf(countDefault).multiply(BigDecimal.valueOf(19.9))));
         map.put(1, new PaymentSummaryTotal(countFallback, BigDecimal.valueOf(countFallback).multiply(BigDecimal.valueOf(19.9))));
-//
-//        for (SummaryPaymentResponse info : res) {
-//            Integer type = info.type();
-//            BigDecimal sum = info.sum();
-//            Long count = info.count();
-//            map.put(type, new PaymentSummaryTotal(count, sum));
-//        }
 
         return map;
     }
--- src/main/java/io/backendscience/rinha_backend_2025_java/application/SavePaymentDefaultUC.java
+++ src/main/java/io/backendscience/rinha_backend_2025_java/application/SavePaymentDefaultUC.java
@@ -1,18 +1,12 @@
 package io.backendscience.rinha_backend_2025_java.application;
 
-import io.backendscience.rinha_backend_2025_java.adapter.inbound.PaymentsController;
 import io.backendscience.rinha_backend_2025_java.adapter.outbound.PaymentProcessorGateway;
-import io.backendscience.rinha_backend_2025_java.adapter.outbound.PaymentRepository;
-import io.backendscience.rinha_backend_2025_java.adapter.outbound.resources.PaymentEntity;
 import io.backendscience.rinha_backend_2025_java.domain.PaymentDetail;
 import io.backendscience.rinha_backend_2025_java.domain.PaymentType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPooled;
 
-import java.time.OffsetDateTime;
-import java.time.ZoneOffset;
 import java.util.logging.Logger;
 
 @Component
@@ -20,7 +14,6 @@ import java.util.logging.Logger;
 public class SavePaymentDefaultUC {
 
     private final PaymentProcessorGateway paymentPort;
-    private final PaymentRepository paymentRepository;
     private final Logger logger = Logger.getLogger(SavePaymentDefaultUC.class.getName());
     private final JedisPooled jedisPooled;
 
@@ -28,18 +21,7 @@ public class SavePaymentDefaultUC {
 
         PaymentType paymentType = paymentPort.savePaymentDefault(paymentDetail);
 
-        PaymentEntity entity = new PaymentEntity();
-        entity.setCorrelationId(paymentDetail.correlationId());
-        entity.setAmount(paymentDetail.amount());
-        entity.setRequestedAt(paymentDetail.requestedAt());
-        entity.setType(paymentType.getValue());
-
-        paymentRepository.save(entity);
-
         jedisPooled.tsAdd("payments:type:" + paymentType.getValue() + ":count", paymentDetail.requestedAt().toInstant().toEpochMilli(), 1);
-//        try (Jedis jedis = jedisPooled.getResource()) {
-//
-//        }
 
         logger.info("Finish saving PAYMENT_DETAIL: " + paymentDetail);
     }
