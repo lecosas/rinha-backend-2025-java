c1ceaf9bd7e2ac0fada2ac0ca12e49bb99eb25b3
lecosas <lecosas@gmail.com>
2025-08-10 07:53:13 -0300

    feat(): Include new configuration

--- src/main/java/io/backendscience/rinha_backend_2025_java/application/service/EnqueuePaymentService.java
+++ src/main/java/io/backendscience/rinha_backend_2025_java/application/service/EnqueuePaymentService.java
@@ -49,7 +49,7 @@ public class EnqueuePaymentService implements EnqueuePaymentUseCase {
             }
         }
 
-        pauseFor(10);
+        pauseFor(exceptionDelay);
         paymentWorker.addToQueue(paymentDetail);
         paymentWorker.startExecution();
     }
