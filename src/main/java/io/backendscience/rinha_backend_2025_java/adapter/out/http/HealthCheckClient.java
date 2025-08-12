2f07787e6a4f65a6300fda94521952d58b7ceb36
lecosas <lecosas@gmail.com>
2025-08-11 21:37:29 -0300

    feat(): Re-create RestClient

--- src/main/java/io/backendscience/rinha_backend_2025_java/adapter/out/http/HealthCheckClient.java
+++ src/main/java/io/backendscience/rinha_backend_2025_java/adapter/out/http/HealthCheckClient.java
@@ -7,54 +7,55 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
+//import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 import java.util.logging.Logger;
 
-@Service
-@RequiredArgsConstructor
-public class HealthCheckClient implements HealthCheckGateway {
+//@Service
+//@RequiredArgsConstructor
+//public class HealthCheckClient implements HealthCheckGateway {
+public class HealthCheckClient {
 
-    @Value("${payment-processor.default-url}")
-    private String paymentProcessorDefaultUrl;
-
-    @Value("${payment-processor.fallback-url}")
-    private String paymentProcessorFallbackUrl;
-
-    private final Logger logger = Logger.getLogger(HealthCheckClient.class.getName());
-    private final WebClient webClient;
-    private final String HEALTH_CHECK_ENDPOINT = "/payments/service-health";
-
-    public HealthCheckStatus getHeathCheckDefault() {
-        return getHealthCheck(paymentProcessorDefaultUrl);
-    }
-
-    public HealthCheckStatus getHeathCheckFallback() {
-        return getHealthCheck(paymentProcessorFallbackUrl);
-    }
-
-    private HealthCheckStatus getHealthCheck(String baseUrl) {
-        ResponseEntity<HealthCheckStatus> healthCheckStatusResponse = webClient
-                .get()
-                .uri(baseUrl + HEALTH_CHECK_ENDPOINT)
-                .retrieve()
-//                .onStatus(HttpStatusCode::is4xxClientError, clientResponse -> {
-//                    logger.severe(String.format("Error %s to get health check status.", clientResponse.statusCode()));
-//                    return Mono.empty();
-//                })
-//                .onStatus(HttpStatusCode::is5xxServerError, clientResponse -> {
-//                    logger.severe(String.format("Error %s to get health check status.", clientResponse.statusCode()));
-//                    return Mono.empty();
-//                })
-                .toEntity(HealthCheckStatus.class)
-                .doOnError(Throwable::printStackTrace)
-                .block();
-
-        if (healthCheckStatusResponse.getStatusCode().is2xxSuccessful()) {
-            return healthCheckStatusResponse.getBody();
-        }
-
-        throw new RuntimeException("Error to get health check status.");
-    }
+//    @Value("${payment-processor.default-url}")
+//    private String paymentProcessorDefaultUrl;
+//
+//    @Value("${payment-processor.fallback-url}")
+//    private String paymentProcessorFallbackUrl;
+//
+//    private final Logger logger = Logger.getLogger(HealthCheckClient.class.getName());
+//    private final WebClient webClient;
+//    private final String HEALTH_CHECK_ENDPOINT = "/payments/service-health";
+//
+//    public HealthCheckStatus getHeathCheckDefault() {
+//        return getHealthCheck(paymentProcessorDefaultUrl);
+//    }
+//
+//    public HealthCheckStatus getHeathCheckFallback() {
+//        return getHealthCheck(paymentProcessorFallbackUrl);
+//    }
+//
+//    private HealthCheckStatus getHealthCheck(String baseUrl) {
+//        ResponseEntity<HealthCheckStatus> healthCheckStatusResponse = webClient
+//                .get()
+//                .uri(baseUrl + HEALTH_CHECK_ENDPOINT)
+//                .retrieve()
+////                .onStatus(HttpStatusCode::is4xxClientError, clientResponse -> {
+////                    logger.severe(String.format("Error %s to get health check status.", clientResponse.statusCode()));
+////                    return Mono.empty();
+////                })
+////                .onStatus(HttpStatusCode::is5xxServerError, clientResponse -> {
+////                    logger.severe(String.format("Error %s to get health check status.", clientResponse.statusCode()));
+////                    return Mono.empty();
+////                })
+//                .toEntity(HealthCheckStatus.class)
+//                .doOnError(Throwable::printStackTrace)
+//                .block();
+//
+//        if (healthCheckStatusResponse.getStatusCode().is2xxSuccessful()) {
+//            return healthCheckStatusResponse.getBody();
+//        }
+//
+//        throw new RuntimeException("Error to get health check status.");
+//    }
 }
--- src/main/java/io/backendscience/rinha_backend_2025_java/adapter/out/http/PaymentProcessorWebClient.java
+++ src/main/java/io/backendscience/rinha_backend_2025_java/adapter/out/http/PaymentProcessorWebClient.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
+//import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 import java.math.BigDecimal;
@@ -17,8 +17,8 @@ import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.logging.Logger;
 
-@Service
-@RequiredArgsConstructor
+//@Service
+//@RequiredArgsConstructor
 public class PaymentProcessorWebClient implements PaymentProcessorGateway {
 
     private final Logger logger = Logger.getLogger(PaymentProcessorWebClient.class.getName());
@@ -29,86 +29,86 @@ public class PaymentProcessorWebClient implements PaymentProcessorGateway {
     @Value("${payment-processor.fallback-url}")
     private String paymentProcessorFallbackUrl;
 
-    private final WebClient webClient;
+//    private final WebClient webClient;
 
     public void sendPaymentToDefault(PaymentDetail paymentDetail, OffsetDateTime requestedAt) {
-        String payToSend = new StringBuilder("{")
-                .append("\"correlationId\":\"")
-                .append(paymentDetail.correlationId())
-                .append("\",")
-                .append("\"amount\":")
-                .append(paymentDetail.amount().toPlainString())
-                .append(",")
-                .append("\"requestedAt\":\"")
-                .append(requestedAt.format(DateTimeFormatter.ISO_INSTANT))
-                .append("\"")
-                .append("}")
-                .toString();
-
-//        PaymentDetailToSend payToSend = new PaymentDetailToSend(
-//                paymentDetail.correlationId(),
-//                paymentDetail.amount(),
-//                requestedAt.format(DateTimeFormatter.ISO_INSTANT));
-
-        ResponseEntity<Void> defaultResponse = webClient
-                .post()
-                .uri(paymentProcessorDefaultUrl + "/payments")
-                .bodyValue(payToSend)
-                .retrieve()
-//                .onStatus(HttpStatusCode::is4xxClientError, clientResponse -> {
-//                    logger.warning(String.format("Error %s to send DEFAULT payment.", clientResponse.statusCode()));
-//                    return Mono.empty();
-//                })
-//                .onStatus(HttpStatusCode::is5xxServerError, clientResponse -> {
-//                    logger.severe(String.format("Error %s to send DEFAULT payment.", clientResponse.statusCode()));
-//                    return Mono.empty();
-//                })
-                .toBodilessEntity()
-                .block();
-
-        if (!defaultResponse.getStatusCode().is2xxSuccessful()) {
-            throw new RuntimeException("Error sending the payment to the FALLBACK");
-        }
+//        String payToSend = new StringBuilder("{")
+//                .append("\"correlationId\":\"")
+//                .append(paymentDetail.correlationId())
+//                .append("\",")
+//                .append("\"amount\":")
+//                .append(paymentDetail.amount().toPlainString())
+//                .append(",")
+//                .append("\"requestedAt\":\"")
+//                .append(requestedAt.format(DateTimeFormatter.ISO_INSTANT))
+//                .append("\"")
+//                .append("}")
+//                .toString();
+//
+////        PaymentDetailToSend payToSend = new PaymentDetailToSend(
+////                paymentDetail.correlationId(),
+////                paymentDetail.amount(),
+////                requestedAt.format(DateTimeFormatter.ISO_INSTANT));
+//
+//        ResponseEntity<Void> defaultResponse = webClient
+//                .post()
+//                .uri(paymentProcessorDefaultUrl + "/payments")
+//                .bodyValue(payToSend)
+//                .retrieve()
+////                .onStatus(HttpStatusCode::is4xxClientError, clientResponse -> {
+////                    logger.warning(String.format("Error %s to send DEFAULT payment.", clientResponse.statusCode()));
+////                    return Mono.empty();
+////                })
+////                .onStatus(HttpStatusCode::is5xxServerError, clientResponse -> {
+////                    logger.severe(String.format("Error %s to send DEFAULT payment.", clientResponse.statusCode()));
+////                    return Mono.empty();
+////                })
+//                .toBodilessEntity()
+//                .block();
+//
+//        if (!defaultResponse.getStatusCode().is2xxSuccessful()) {
+//            throw new RuntimeException("Error sending the payment to the FALLBACK");
+//        }
     }
 
     public void sendPaymentToFallback(PaymentDetail paymentDetail, OffsetDateTime requestedAt) {
-        String payToSend = new StringBuilder("{")
-                .append("\"correlationId\":\"")
-                .append(paymentDetail.correlationId())
-                .append("\",")
-                .append("\"amount\":")
-                .append(paymentDetail.amount().toPlainString())
-                .append(",")
-                .append("\"requestedAt\":\"")
-                .append(requestedAt.format(DateTimeFormatter.ISO_INSTANT))
-                .append("\"")
-                .append("}")
-                .toString();
+//        String payToSend = new StringBuilder("{")
+//                .append("\"correlationId\":\"")
+//                .append(paymentDetail.correlationId())
+//                .append("\",")
+//                .append("\"amount\":")
+//                .append(paymentDetail.amount().toPlainString())
+//                .append(",")
+//                .append("\"requestedAt\":\"")
+//                .append(requestedAt.format(DateTimeFormatter.ISO_INSTANT))
+//                .append("\"")
+//                .append("}")
+//                .toString();
 
         //        PaymentDetailToSend payToSend = new PaymentDetailToSend(
         //                paymentDetail.correlationId(),
         //                paymentDetail.amount(),
         //                requestedAt.format(DateTimeFormatter.ISO_INSTANT));
 
-        ResponseEntity<Void> fallbackResponse = webClient
-                .post()
-                .uri(paymentProcessorFallbackUrl + "/payments")
-                .bodyValue(payToSend)
-                .retrieve()
-//                .onStatus(HttpStatusCode::is4xxClientError, clientResponse -> {
-//                    logger.warning(String.format("Error %s to send FALLBACK payment.", clientResponse.statusCode()));
-//                    return Mono.empty();
-//                })
-//                .onStatus(HttpStatusCode::is5xxServerError, clientResponse -> {
-//                    logger.severe(String.format("Error %s to send FALLBACK payment.", clientResponse.statusCode()));
-//                    return Mono.empty();
-//                })
-                .toBodilessEntity()
-                .block();
-
-        if (!fallbackResponse.getStatusCode().is2xxSuccessful()) {
-            throw new RuntimeException("Error sending the payment to the FALLBACK");
-        }
+//        ResponseEntity<Void> fallbackResponse = webClient
+//                .post()
+//                .uri(paymentProcessorFallbackUrl + "/payments")
+//                .bodyValue(payToSend)
+//                .retrieve()
+////                .onStatus(HttpStatusCode::is4xxClientError, clientResponse -> {
+////                    logger.warning(String.format("Error %s to send FALLBACK payment.", clientResponse.statusCode()));
+////                    return Mono.empty();
+////                })
+////                .onStatus(HttpStatusCode::is5xxServerError, clientResponse -> {
+////                    logger.severe(String.format("Error %s to send FALLBACK payment.", clientResponse.statusCode()));
+////                    return Mono.empty();
+////                })
+//                .toBodilessEntity()
+//                .block();
+//
+//        if (!fallbackResponse.getStatusCode().is2xxSuccessful()) {
+//            throw new RuntimeException("Error sending the payment to the FALLBACK");
+//        }
     }
 
     private record PaymentDetailToSend(
